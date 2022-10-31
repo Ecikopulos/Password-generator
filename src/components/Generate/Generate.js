f0dfd5388082325a3bdf6bbf68bea0b3c9224604
Ecikopulos <kezus777@gmail.com>
2022-10-31 15:27:10 +0100

    // done

---
+++ src/components/Generate/Generate.js
@@ -0,0 +1,72 @@
+import React, { useContext } from "react";
+import { Wrapper, Text, Icon } from "./Generate.styles";
+import { Context } from "../../assets/Context/Context";
+
+const Allowed = {
+  uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
+  lowers: "qwertyuiopasdfghjklzxcvbnm",
+  numbers: "1234567890",
+  symbols: "!@#$%^&*()_+~`|}{[]\\:;?><,./-=",
+};
+
+const Generate = () => {
+  const {
+    currentValue,
+    upperValue,
+    lowerValue,
+    numberValue,
+    symbolValue,
+    setGeneratedPass,
+  } = useContext(Context);
+
+  const getRandomCharFromString = (str) =>
+    str.charAt(Math.floor(Math.random() * str.length));
+
+  const handleGeneratePass = (
+    currentValue,
+    upperValue,
+    lowerValue,
+    numberValue,
+    symbolValue
+  ) => {
+    let pass = "";
+    let newAllowed = { ...Allowed };
+
+    if (!upperValue) {
+      delete newAllowed.uppers;
+    }
+    if (!lowerValue) {
+      delete newAllowed.lowers;
+    }
+    if (!numberValue) {
+      delete newAllowed.numbers;
+    }
+    if (!symbolValue) {
+      delete newAllowed.symbols;
+    }
+
+    for (let i = 0; i < currentValue; i++) {
+      pass += getRandomCharFromString(Object.values(newAllowed).join(""));
+    }
+    return setGeneratedPass(pass);
+  };
+
+  return (
+    <Wrapper
+      onClick={() =>
+        handleGeneratePass(
+          currentValue,
+          upperValue,
+          lowerValue,
+          numberValue,
+          symbolValue
+        )
+      }
+    >
+      <Text>generate</Text>
+      <Icon />
+    </Wrapper>
+  );
+};
+
+export default Generate;
--- src/components/GeneratePassArea/GeneratePassArea.js
+++ src/components/GeneratePassArea/GeneratePassArea.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Wrapper } from "./GeneratePassArea.styles";
 import Slider from "../Slider/Slider";
 import CheckBoxesArea from "../CheckBoxesArea/CheckBoxesArea";
-import Button from "../Button/Button";
+import Generate from "../Generate/Generate";
 import PasswordStrengthArea from "../PasswordStrengthArea/PasswordStrengthArea";
 
 const GeneratePassArea = () => {
@@ -12,7 +12,7 @@ const GeneratePassArea = () => {
       <Slider />
       <CheckBoxesArea setPower={setPasswordPower} />
       <PasswordStrengthArea power={passwordPower} />
-      <Button />
+      <Generate />
     </Wrapper>
   );
 };
